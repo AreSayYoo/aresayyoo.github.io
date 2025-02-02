e6fbae45ba1b0a5507ce3ccbfa7e291c57ada22a
Matt Arceo <32740884+AreSayYoo@users.noreply.github.com>
2025-02-01 23:57:23 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // Required for static export
-  basePath: 'aresayyoo.github.io', // Replace with your actual GitHub repo name
+  basePath: '/aresayyoo.github.io', // Replace with your actual GitHub repo name
   assetPrefix: '/aresayyoo.github.io/', // Ensures correct asset loading
   images: {
     unoptimized: true, // Needed if using next/image
