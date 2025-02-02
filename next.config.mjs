9571683dc14acb8394319d58a6803af0e3004525
Matt Arceo <32740884+AreSayYoo@users.noreply.github.com>
2025-02-02 00:55:53 -0500

    fixed config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // Required for static export
-  basePath: '/', // Replace with your actual GitHub repo name
-  assetPrefix: '/', // Ensures correct asset loading
+  basePath: '', // Replace with your actual GitHub repo name
+  assetPrefix: '', // Ensures correct asset loading
   images: {
     unoptimized: true, // Needed if using next/image
   },
